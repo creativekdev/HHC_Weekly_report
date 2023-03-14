8c88e9e15d9e14c0151c8c6d0d5b3d222a342f37
creativekdev <creativekdev@gmail.com>
2023-03-14 12:37:29 +0100

    image size

--- resources/views/content/nurse/report.blade.php
+++ resources/views/content/nurse/report.blade.php
@@ -54,16 +54,17 @@ Address: <u>
 <h2 align=center style='text-align:center'><span style='font-size:14.0pt;
 mso-fareast-font-family:"Times New Roman"'>PLEASE SIGN FOR ONLY ONE VISIT AT A
 TIME<o:p></o:p></span></h2>
-<table class=MsoNormalTable border=1 cellspacing=0 cellpadding=0 width="103%"
+
+ <table class=MsoNormalTable border=1 cellspacing=0 cellpadding=0 width="103%"
  style='width:103.14%;border-collapse:collapse;border:none;mso-border-alt:solid windowtext .5pt;
  mso-yfti-tbllook:1184;mso-padding-alt:0in 5.4pt 0in 5.4pt'>
  <tr style='mso-yfti-irow:0;mso-yfti-firstrow:yes;height:26.45pt'>
-  <td width=47 valign=top style='width:30.85pt;border:solid windowtext 1.0pt;
+  <td width=59 valign=top style='width:38.9pt;border:solid windowtext 1.0pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>Day<o:p></o:p></span></p>
   </td>
-  <td width=55 valign=top style='width:36.1pt;border:solid windowtext 1.0pt;
+  <td width=54 valign=top style='width:35.9pt;border:solid windowtext 1.0pt;
   border-left:none;mso-border-left-alt:solid windowtext .5pt;mso-border-alt:
   solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
@@ -76,34 +77,34 @@ TIME<o:p></o:p></span></h2>
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>Visit
   Code<o:p></o:p></span></p>
   </td>
-  <td width=92 valign=top style='width:60.8pt;border:solid windowtext 1.0pt;
+  <td width=73 valign=top style='width:48.45pt;border:solid windowtext 1.0pt;
   border-left:none;mso-border-left-alt:solid windowtext .5pt;mso-border-alt:
   solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>N/C
   Code<o:p></o:p></span></p>
   </td>
-  <td width=82 valign=top style='width:.75in;border:solid windowtext 1.0pt;
+  <td width=96 valign=top style='width:63.55pt;border:solid windowtext 1.0pt;
   border-left:none;mso-border-left-alt:solid windowtext .5pt;mso-border-alt:
   solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>Time
   In<o:p></o:p></span></p>
   </td>
-  <td width=95 valign=top style='width:63.0pt;border:solid windowtext 1.0pt;
+  <td width=93 valign=top style='width:61.3pt;border:solid windowtext 1.0pt;
   border-left:none;mso-border-left-alt:solid windowtext .5pt;mso-border-alt:
   solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>Time
   Out<o:p></o:p></span></p>
   </td>
-  <td width=68 valign=top style='width:45.0pt;border:solid windowtext 1.0pt;
+  <td width=67 valign=top style='width:44.45pt;border:solid windowtext 1.0pt;
   border-left:none;mso-border-left-alt:solid windowtext .5pt;mso-border-alt:
   solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>Units<o:p></o:p></span></p>
   </td>
-  <td width=192 valign=top style='width:126.95pt;border:solid windowtext 1.0pt;
+  <td width=188 valign=top style='width:124.15pt;border:solid windowtext 1.0pt;
   border-left:none;mso-border-left-alt:solid windowtext .5pt;mso-border-alt:
   solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
@@ -117,13 +118,13 @@ TIME<o:p></o:p></span></h2>
  @foreach($visits as $visit)
 <?php $i++ ?>
 <tr style='mso-yfti-irow:1;height:26.45pt'>
-  <td width=47 valign=top style='width:30.85pt;border:solid windowtext 1.0pt;
+  <td width=59 valign=top style='width:38.9pt;border:solid windowtext 1.0pt;
   border-top:none;mso-border-top-alt:solid windowtext .5pt;mso-border-alt:solid windowtext .5pt;
   padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>{{$visit->day}}<o:p></o:p></span></p>
   </td>
-  <td width=55 valign=top style='width:36.1pt;border-top:none;border-left:none;
+  <td width=54 valign=top style='width:35.9pt;border-top:none;border-left:none;
   border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
@@ -137,57 +138,58 @@ TIME<o:p></o:p></span></h2>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>{{$visit->visit_code}}<o:p></o:p></span></p>
   </td>
-  <td width=92 valign=top style='width:60.8pt;border-top:none;border-left:none;
-  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  <td width=73 valign=top style='width:48.45pt;border-top:none;border-left:
+  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'><o:p>&nbsp;</o:p></span></p>
   </td>
-  <td width=82 valign=top style='width:.75in;border-top:none;border-left:none;
-  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  <td width=96 valign=top style='width:63.55pt;border-top:none;border-left:
+  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>{{$visit->time_in}}<o:p></o:p></span></p>
   </td>
-  <td width=95 valign=top style='width:63.0pt;border-top:none;border-left:none;
+  <td width=93 valign=top style='width:61.3pt;border-top:none;border-left:none;
   border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>{{$visit->time_out}}<o:p></o:p></span></p>
   </td>
-  <td width=68 valign=top style='width:45.0pt;border-top:none;border-left:none;
-  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  <td width=67 valign=top style='width:44.45pt;border-top:none;border-left:
+  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'><o:p>&nbsp;</o:p></span></p>
   </td>
-  <td width=192 valign=top style='width:126.95pt;border-top:none;border-left:
+  <td width=188 valign=top style='width:124.15pt;border-top:none;border-left:
   none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman";
-  mso-no-proof:yes'><img width=100 height=30 id="_x0000_i1029"
+  mso-no-proof:yes'><img width=100 height=30 id="_x0000_i1038"
   src="{{route('getImage', ['id'=>$visit->id])}}"
   alt="{{route('getImage', ['id'=>$visit->id])}}"></span><span
   style='mso-fareast-font-family:"Times New Roman"'><o:p></o:p></span></p>
   </td>
  </tr>
+ 
+
 @endforeach
 @for(;$i<14;$i++)
-
 <tr style='mso-yfti-irow:1;height:26.45pt'>
-  <td width=47 valign=top style='width:30.85pt;border:solid windowtext 1.0pt;
+  <td width=59 valign=top style='width:38.9pt;border:solid windowtext 1.0pt;
   border-top:none;mso-border-top-alt:solid windowtext .5pt;mso-border-alt:solid windowtext .5pt;
   padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'><o:p></o:p></span></p>
   </td>
-  <td width=55 valign=top style='width:36.1pt;border-top:none;border-left:none;
+  <td width=54 valign=top style='width:35.9pt;border-top:none;border-left:none;
   border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
@@ -201,37 +203,35 @@ TIME<o:p></o:p></span></h2>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'><o:p></o:p></span></p>
   </td>
-  <td width=92 valign=top style='width:60.8pt;border-top:none;border-left:none;
-  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  <td width=73 valign=top style='width:48.45pt;border-top:none;border-left:
+  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'><o:p>&nbsp;</o:p></span></p>
   </td>
-  <td width=82 valign=top style='width:.75in;border-top:none;border-left:none;
+  <td width=96 valign=top style='width:63.55pt;border-top:none;border-left:
+  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
+  mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
+  <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
+  auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'><o:p></o:p></span></p>
+  </td>
+  <td width=93 valign=top style='width:61.3pt;border-top:none;border-left:none;
   border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
-  auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>
-  <o:p></o:p></span></p>
+  auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'><o:p></o:p></span></p>
   </td>
-  <td width=95 valign=top style='width:63.0pt;border-top:none;border-left:none;
-  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
-  mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
-  <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
-  auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'>
-  <o:p></o:p></span></p>
-  </td>
-  <td width=68 valign=top style='width:45.0pt;border-top:none;border-left:none;
-  border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
+  <td width=67 valign=top style='width:44.45pt;border-top:none;border-left:
+  none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
   <p class=MsoNormal align=center style='mso-margin-top-alt:auto;mso-margin-bottom-alt:
   auto;text-align:center'><span style='mso-fareast-font-family:"Times New Roman"'><o:p>&nbsp;</o:p></span></p>
   </td>
-  <td width=192 valign=top style='width:126.95pt;border-top:none;border-left:
+  <td width=188 valign=top style='width:124.15pt;border-top:none;border-left:
   none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:solid windowtext .5pt;
   mso-border-alt:solid windowtext .5pt;padding:0in 5.4pt 0in 5.4pt;height:26.45pt'>
@@ -241,6 +241,8 @@ TIME<o:p></o:p></span></h2>
   style='mso-fareast-font-family:"Times New Roman"'><o:p></o:p></span></p>
   </td>
  </tr>
+ 
+
 @endfor
 
  
